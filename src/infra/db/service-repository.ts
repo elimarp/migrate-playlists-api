6dcf9ddb8b8ea1f9a9edaa832322974a9d23c599
Eli Pereira <dev.elimarp@gmail.com>
2024-02-05 16:47:46 -0300

    feat: add service repository

---
+++ src/infra/db/service-repository.ts
@@ -0,0 +1,16 @@
+import { type GetServicesRepository } from '../../data/protocols/db/get-services-repository'
+import { type ServiceModel } from '../../domain/models/service'
+import { mongodbClient } from './mongodb-client'
+
+export class ServiceRepository implements GetServicesRepository {
+  async getAll (): Promise<GetServicesRepository.Result> {
+    const serviceCollection = mongodbClient.getCollection('services')
+    const services = await serviceCollection.find().toArray()
+
+    const result = services.map(
+      ({ _id, ...rest }) =>
+        ({ id: String(_id), ...rest })) as unknown as ServiceModel[]
+
+    return result
+  }
+}
