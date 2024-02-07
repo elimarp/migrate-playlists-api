75cb306fdccfb9a2ad021e1bbf1190b9de324576
Eli Pereira <dev.elimarp@gmail.com>
2024-02-07 18:37:35 -0300

    feat: add get user playlists controller

---
+++ src/presentation/controllers/get-user-playlists.ts
@@ -0,0 +1,49 @@
+import { badRequest, forbidden, ok, serverError, unauthorized } from '../helpers/http'
+import { type HttpRequest, type HttpResponse } from '../protocols/http'
+import { type Controller } from './controller'
+
+import { type GetUserPlaylistsProtocol } from '../../domain/usecases/service/get-user-playlists'
+import { type ValidateTokenProtocol } from '../../domain/usecases/security/validate-token'
+import { AccessTokenExpiredError } from '../../infra/helpers/exceptions'
+
+export class GetUserPlaylistsController implements Controller {
+  constructor (
+    private readonly usecases: Record<string, GetUserPlaylistsProtocol>,
+    private readonly validateToken: ValidateTokenProtocol
+  ) {}
+
+  async handle (request: HttpRequest<any>): Promise<HttpResponse> {
+    try {
+      if (!request.headers.authorization) return forbidden()
+      const session = await this.validateToken.validate(request.headers.authorization)
+
+      if (!request.path) return badRequest({})
+      const { userId, serviceKeyword } = request.path
+
+      if (!userId) return badRequest({ message: 'missing userId' })
+      if (!serviceKeyword) return badRequest({ message: 'missing service' })
+
+      if (!this.usecases[serviceKeyword]) return badRequest({ message: 'feature not available for this service' })
+      const sessionService = session.services.find(service => service.keyword === serviceKeyword)
+
+      if (!sessionService) return forbidden('you are not authenticated to this service')
+
+      const { limit, offset, payload, total } = await this.usecases[serviceKeyword].getUserPlaylists({
+        serviceAccessToken: sessionService.accessToken,
+        userId
+        // TODO: limit and offset
+      })
+
+      return ok({
+        message: 'Here are your playlists',
+        payload, // TODO: I think usecase should call it playlists rather than payload
+        total,
+        limit,
+        offset
+      })
+    } catch (error) {
+      if (error instanceof AccessTokenExpiredError) return unauthorized('accessToken expired')
+      return serverError()
+    }
+  }
+}
