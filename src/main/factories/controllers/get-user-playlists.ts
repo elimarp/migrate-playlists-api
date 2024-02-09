d71e16fe800c2b50103fb54c2d46de34a805b560
Eli Pereira <dev.elimarp@gmail.com>
2024-02-09 18:21:26 -0300

    fix: add RequestValidator to get user playlists factory

--- src/main/factories/controllers/get-user-playlists.ts
+++ src/main/factories/controllers/get-user-playlists.ts
@@ -5,12 +5,20 @@ import { HttpHelper } from '../../../infra/helpers/http-helper'
 import { JwtHelper } from '../../../infra/helpers/jwt-helper'
 import { SpotifyService } from '../../../infra/http/spotify/spotify-service'
 import { GetUserPlaylistsController } from '../../../presentation/controllers/playlist/get-user-playlists'
+import { RequestValidator } from '../../../presentation/helpers/request-validator'
+import { getUserPlaylistsValidation } from '../../../presentation/helpers/request-validators/playlist/get-user-playlists'
 import { constants } from '../../../utils/constants'
 
 const usecases = {
-  spotify: new GetSpotifyUserPlaylists(new SpotifyService(new HttpHelper(constants.http.spotify.BASE_URL)))
+  spotify: new GetSpotifyUserPlaylists(
+    new SpotifyService(new HttpHelper(constants.http.spotify.BASE_URL))
+  )
 }
 
 export const makeGetUserPlaylistsController = () => {
-  return new GetUserPlaylistsController(usecases, new ValidateToken(new JwtHelper(), new SessionRepository()))
+  return new GetUserPlaylistsController(
+    new ValidateToken(new JwtHelper(), new SessionRepository()),
+    new RequestValidator(getUserPlaylistsValidation),
+    usecases
+  )
 }
